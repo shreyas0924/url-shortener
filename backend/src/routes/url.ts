b7cedbd95ed736ad30af908bb52ed57757a54411
shreyas0924 <shreyasp0924@gmail.com>
2024-03-09 23:32:34 +0530

    changed fe and be dirs

--- backend/src/routes/url.ts
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { handleNewShortUrl } from "../controllers/url";
-const router = express.Router();
-
-export const urlRouter = router.post("/", handleNewShortUrl);
